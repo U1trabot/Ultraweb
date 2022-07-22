b6c42579179c5debc896034184f26e472ce2b8b6
Ultrabot <em21744@bristol.ac.uk>
2022-07-22 22:47:12 +0100

    Update loader.js

--- cog/loader.js
+++ cog/loader.js
@@ -1,5 +1,5 @@
 function loadGames() {
-  for (game in gamelist.games) {
+  for (game in gamelist) {
     document.getElementById(list).innerHTML += '<li><a href="'+game.link+'">'+game.name+'</a></li>'
   }
 }
